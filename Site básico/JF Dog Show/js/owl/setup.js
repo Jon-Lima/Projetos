4ca51c4d31701537d9f4803c7c2a0a49524a619b
Jonathan Lima <76965116+Jon-Lima@users.noreply.github.com>
2021-02-17 12:43:54 -0300

    Add files via upload

---
+++ Site básico/JF Dog Show/js/owl/setup.js
@@ -0,0 +1,31 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function redirecionar(){
+    window.open("https://www.facebook.com/Canil-Escola-JF-Dog-Show-384960418273338");
+    //window.location.href = "https://www.linkedin.com/in/jonathan-lima-b2ab70193/";
+}
+
+function redirecionar2(){
+    window.open("https://twitter.com/Jonllima");
+    //window.location.href = "https://www.linkedin.com/in/jonathan-lima-b2ab70193/";
+}
+
+function redirecionar3(){
+    window.open("https://www.instagram.com/farleyplima/");
+    //window.location.href = "https://www.linkedin.com/in/jonathan-lima-b2ab70193/";
+}
